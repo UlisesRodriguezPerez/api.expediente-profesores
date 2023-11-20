02b91b13a43b7a3a4d26c14e1ff6d3951b8f577f
DavidEspinozaNemo <david1998es16@gmail.com>
2023-11-20 06:22:24 -0600

    Update Publication.php

--- app/Models/Publication.php
+++ app/Models/Publication.php
@@ -17,7 +17,6 @@ class Publication extends Model
         'name',
         'coauthors',
         'objectives',
-        'goals',
         'dissemination_medium',
         'ORCID'
     ];
@@ -28,7 +27,6 @@ class Publication extends Model
         'name',
         'coauthors',
         'objectives',
-        'goals',
         'dissemination_medium',
         'ORCID',
         'collaborator_id',
